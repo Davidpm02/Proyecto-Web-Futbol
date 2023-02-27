f45c1026d9b11c57bd70016969809272a1d24edb
David Padilla <109883212+Davidpm02@users.noreply.github.com>
2023-02-27 01:06:58 +0100

    COMMIT #3

--- Proyecto_Web_Futbol_FLASK/app/routes.py
+++ Proyecto_Web_Futbol_FLASK/app/routes.py
@@ -1,6 +1,7 @@
 # Este archivo contiene el código que define las rutas y controladores para la aplicación.
 
 from flask import Blueprint
+from flask import render_template
 
 # Crear un objeto Blueprint para las rutas
 main = Blueprint('main', __name__)
@@ -8,7 +9,7 @@ main = Blueprint('main', __name__)
 # Definir una ruta
 @main.route('/')
 def index():
-    return '¡Hola, mundo!'
+    return render_template('index.html')
 
 # Definir otra ruta
 @main.route('/about')
@@ -16,3 +17,7 @@ def about():
     return 'Acerca de nosotros'
 
 # Agregar más rutas aquí...
+
+
+#Se ha creado un objeto Blueprint llamado main que se utilizará para agrupar las rutas de la aplicación. 
+# Luego, se definen dos rutas utilizando el decorador @main.route(). 
